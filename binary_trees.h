cf00016a5c9137a585abf537678dc7ef0642a325
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-08-27 19:44:37 +0100

    Task1 Insert left

--- binary_trees.h
+++ binary_trees.h
@@ -28,5 +28,7 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
 #endif /* _BINARY_TREES_H_ */
