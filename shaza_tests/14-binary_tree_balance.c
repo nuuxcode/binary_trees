04cc830210c74e308836d4d7eb1435138051d555
shazaaly <shaza.aly@gmail.com>
2023-08-30 18:48:55 +0300

    balance

--- shaza_tests/14-binary_tree_balance.c
+++ shaza_tests/14-binary_tree_balance.c
@@ -1,6 +1,12 @@
 #include "binary_trees.h"
 size_t binary_tree_height(const binary_tree_t *tree);
-
+/**
+* binary_tree_balance - Measures the balance factor of a binary tree.
+*
+* @tree: Pointer to the root node of the tree to measure balance.
+*
+* Return: Balance factor of the tree, or 0 if @tree is NULL.
+*/
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int right_height, left_height, balance;
@@ -18,6 +24,13 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 }
 
+/**
+* binary_tree_height - Measures the height of a binary tree.
+*
+* @tree: Pointer to the root node of the tree to measure.
+*
+* Return: Height of the tree, or 0 if @tree is NULL.
+*/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t right_count;
