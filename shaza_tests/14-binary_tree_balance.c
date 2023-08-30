f68601d8452068b93e3a46e343c07f1bd39e829c
shazaaly <shaza.aly@gmail.com>
2023-08-30 18:47:49 +0300

    14. Balance factor

---
+++ shaza_tests/14-binary_tree_balance.c
@@ -0,0 +1,38 @@
+#include "binary_trees.h"
+size_t binary_tree_height(const binary_tree_t *tree);
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int right_height, left_height, balance;
+
+	if (!tree)
+	{
+		return (0);
+	}
+
+	right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	balance = left_height - right_height;
+
+	return (balance);
+
+}
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t right_count;
+	size_t left_count;
+
+	if (!tree)
+	{
+		return (0);
+	}
+	right_count = binary_tree_height(tree->right);
+	left_count = binary_tree_height(tree->left);
+	if (right_count > left_count)
+	{
+		return (right_count + 1);
+	}
+	return (left_count + 1);
+
+}
